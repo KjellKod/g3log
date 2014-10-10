56d1a94b914a74334ab28ec77f1c42d125aba36a
KjellKod <hedstrom@kjellkod.cc>
2014-10-10 00:45:38 -0600

    Fixed script

--- scripts/buildtravis.sh
+++ scripts/buildtravis.sh
@@ -2,8 +2,8 @@
 
 set -ev
 
-unzip -fo 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
-mkdir build_travis
+unzip -o 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
+mkdir  build_travis
 cd build_travis
 cmake -DUSE_G3LOG_UNIT_TEST=ON ..
 make -j
