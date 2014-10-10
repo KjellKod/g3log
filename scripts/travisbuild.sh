01a3efcf7d013d50f9cb434c393f1c56eebae959
KjellKod <hedstrom@kjellkod.cc>
2014-10-09 23:49:51 -0600

    Updated build script

--- scripts/travisbuild.sh
+++ scripts/travisbuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -ev
-cd ..
-unzip 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
-mkdir travisbuild
-cd travisbuild
+
+unzip -fo 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
+mkdir build_travis
+cd build_travis
 cmake -DUSE_G3LOG_UNIT_TEST=ON ..
 make -j
 ./test_concept_sink
