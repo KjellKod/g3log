e49b1dc15d636a50e93153b2d4baddb5c0cf908d
KjellKod <hedstrom@kjellkod.cc>
2014-10-11 22:49:25 -0600

    wrong compiler test

--- scripts/buildAndRunTests.sh
+++ scripts/buildAndRunTests.sh
@@ -6,8 +6,9 @@ unzip -o 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
 mkdir  build_travis
 cd build_travis
 echo $TRAVIS_OS_NAME
+echo $CXX
 
-if [ "$CXX" = "g++" ]
+if [ "$CXX" = "g++-4.8" ]
 then
    echo "Testing with g++"
    cmake -DCMAKE_CXX_COMPILER=g++ -DUSE_G3LOG_UNIT_TEST=ON ..
@@ -20,7 +21,7 @@ then
    ./test_sink
 fi
 
-if [ "$CXX" = "clang++" ]
+if [ "$CXX" = "clang++-3.4" ]
 then
     echo "Testing with Clang++"
     cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-std=gnu++11 -DUSE_G3LOG_UNIT_TEST=ON ..
