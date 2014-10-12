21edd3fc052701f5b4b3bb9c3ab2753036511be5
KjellKod <hedstrom@kjellkod.cc>
2014-10-11 21:36:39 -0600

    g++ and clang but only on Linux

--- scripts/buildAndRunTests.sh
+++ scripts/buildAndRunTests.sh
@@ -5,26 +5,28 @@ set -ev
 unzip -o 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
 mkdir  build_travis
 cd build_travis
-cmake -DCMAKE_CXX_COMPILER=g++ -DUSE_G3LOG_UNIT_TEST=ON ..
-make -j
-echo "Testing with g++"
-./test_concept_sink
-./test_configuration
-./test_dynamic_loaded_shared_lib
-./test_filechange
-./test_io
-./test_sink
+echo $TRAVIS_OS_NAME
 
+if [ "$CXX" = "g++" ]; then
+   cmake -DCMAKE_CXX_COMPILER=g++ -DUSE_G3LOG_UNIT_TEST=ON ..
+   make -j
+   echo "Testing with g++"
+   ./test_concept_sink
+   ./test_configuration
+   ./test_dynamic_loaded_shared_lib
+   ./test_filechange
+   ./test_io
+   ./test_sink
+fi
 
-echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-echo "Testing with Clang++"
-make clean
-rm -rf *Make* *make* *.so  *3log*
-cmake -DCMAKE_CXX_COMPILER=clang++ -DUSE_G3LOG_UNIT_TEST=ON ..
-make -j
-./test_concept_sink
-./test_configuration
-#./test_dynamic_loaded_shared_lib
-./test_filechange
-./test_io
-./test_sink
+if [ "$CXX" = "clang++"  ]; then
+    echo "Testing with Clang++"
+    cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-std=gnu++11 -DUSE_G3LOG_UNIT_TEST=ON ..
+    make -j
+    ./test_concept_sink
+    ./test_configuration
+    #./test_dynamic_loaded_shared_lib
+    ./test_filechange
+    ./test_io
+    ./test_sink
+ fi
