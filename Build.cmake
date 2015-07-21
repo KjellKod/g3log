6ff84e092b91247b938602f2e71336981d185b13
Alexandre Petitjean <alpetitjean@gmail.com>
2015-07-21 12:21:31 +0200

    Fix path removal for windows

--- Build.cmake
+++ Build.cmake
@@ -69,9 +69,9 @@ ENDIF()
    #MESSAGE(" HEADER FILES ARE: ${HEADER_FILES}")
 
    IF (MSVC OR MINGW) 
-         list(REMOVE_ITEM SRC_FILES  ${LOG_SRC}/g3log/crashhandler_unix.cpp)
-   ELSE()     
-         list(REMOVE_ITEM SRC_FILES  ${LOG_SRC}/crashhandler_windows.cpp ${LOG_SRC}/g3log/stacktrace_windows.hpp ${LOG_SRC}/stacktrace_windows.cpp)
+         list(REMOVE_ITEM SRC_FILES  ${LOG_SRC}/crashhandler_unix.cpp)
+   ELSE()
+         list(REMOVE_ITEM SRC_FILES  ${LOG_SRC}/crashhandler_windows.cpp ${LOG_SRC}/stacktrace_windows.hpp ${LOG_SRC}/stacktrace_windows.cpp)
    ENDIF (MSVC OR MINGW)
 
    set(SRC_FILES ${SRC_FILES} ${SRC_PLATFORM_SPECIFIC})
