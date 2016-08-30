c4343b04cfa2fe946551c38eabd26ddc6c85c1ec
Lynn <lynn@foldr.moe>
2016-08-30 18:05:32 +0200

    typo fix (Vaulue → Value)

--- src/loglevels.cpp
+++ src/loglevels.cpp
@@ -40,7 +40,7 @@ namespace g3 {
       }
 
 #ifdef G3_DYNAMIC_LOGGING
-      std::map<int, atomicbool> g_log_level_status = {{g3::kDebugVaulue, true}, {INFO.value, true}, {WARNING.value, true}, {FATAL.value, true}};
+      std::map<int, atomicbool> g_log_level_status = {{g3::kDebugValue, true}, {INFO.value, true}, {WARNING.value, true}, {FATAL.value, true}};
 #endif
    } // internal
 
@@ -61,7 +61,7 @@ namespace g3 {
 
       void reset() {
          internal::g_log_level_status.clear();
-         internal::g_log_level_status = std::map<int, atomicbool>{{g3::kDebugVaulue, true}, {INFO.value, true}, {WARNING.value, true}, {FATAL.value, true}};
+         internal::g_log_level_status = std::map<int, atomicbool>{{g3::kDebugValue, true}, {INFO.value, true}, {WARNING.value, true}, {FATAL.value, true}};
       }
    } // only_change_at_initialization
 #endif
