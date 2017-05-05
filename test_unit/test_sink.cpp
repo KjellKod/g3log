53b81b52fc751e07d70a9d9d424f42cb367ec120
Andreas Schönle <a.schoenle@abberior-instruments.com>
2017-05-05 15:48:03 +0200

    DBUG vs DEBUG fix moved to generated_definitions.hpp

--- test_unit/test_sink.cpp
+++ test_unit/test_sink.cpp
@@ -68,7 +68,7 @@ TEST(ConceptSink, OneHundredSinks) {
          // ignore the handle
          worker->addSink(std2::make_unique<ScopedSetTrue>(flag, count), &ScopedSetTrue::ReceiveMsg);
       }
-      LOG(DEBUG) << "start message";
+      LOG(G3LOG_DEBUG) << "start message";
       LogMessagePtr message1{std2::make_unique<LogMessage>("test", 0, "test", DEBUG)};
       LogMessagePtr message2{std2::make_unique<LogMessage>("test", 0, "test", DEBUG)};
       auto& write1 = message1.get()->write();
