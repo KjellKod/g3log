d0826d9472b9503f810134afa67cd467a7a7cc44
Andreas Schönle <a.schoenle@abberior-instruments.com>
2017-05-19 08:51:52 +0200

    Fix to_system_time

--- src/g3log/time.hpp
+++ src/g3log/time.hpp
@@ -60,7 +60,10 @@ namespace g3 {
    inline system_time_point to_system_time(const high_resolution_time_point& ts)
    {
 	   using namespace std::chrono;
-	   return time_point_cast<system_clock::duration>(system_clock::now() + (ts - high_resolution_clock::now()));
+	   static auto hrs_now = high_resolution_clock::now();
+	   static auto sys_now = system_clock::now();
+
+	   return time_point_cast<system_clock::duration>(sys_now + (ts - hrs_now));
    }
 }
 
