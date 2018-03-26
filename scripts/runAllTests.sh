e46933ff0f3e6b3d6a5aa484c0d98bbc43ffb371
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2018-03-25 21:46:00 -0600

    improved runalltests

--- scripts/runAllTests.sh
+++ scripts/runAllTests.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 set -v
-#set -x
-
 
 # test_execs=`find ./test_* -perm /u=x` 
 test_execs=`find ./test_* -type f -perm +ugo+x -print`
 
 echo "Tests to run: $test_execs"
-
 while read -r testsuite; do
     ./"$testsuite"
     if [ "$?" -ne 0 ]; then
@@ -17,15 +14,3 @@ while read -r testsuite; do
     fi
 done <<< "$test_execs"
 
-
-#option 2
-#find ./test_* -perm /u=x -exec '{}' ';'
-
-# option 3
-#./test_concept_sink && \
-#./test_cpp_future_concepts && \
-#./test_dynamic_loaded_shared_lib | true && \
-#./test_filechange && \
-#./test_io && \
-#./test_sink && \
-#./test_message
