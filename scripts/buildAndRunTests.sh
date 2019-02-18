1eb0408347c9f98a11e21e4df1965d0e8091e8a1
Kjell Hedström <hedstrom@kjellkod.cc>
2019-02-18 10:50:37 -0700

    Simplified travis setup with ubuntu xenail (#300)
    * test in progress
    
    * improved PR instructions
    
    * revert back

--- scripts/buildAndRunTests.sh
+++ scripts/buildAndRunTests.sh
@@ -7,7 +7,7 @@ unzip -o 3rdParty/gtest/gtest-1.7.0.zip -d 3rdParty/gtest
 
 mkdir -p  build_travis
 cd build_travis
-cmake  -DCMAKE_CXX_FLAGS=-std=c++14 -DADD_G3LOG_UNIT_TEST=ON ..
+cmake  -DCMAKE_CXX_FLAGS=-std=c++14 -DADD_G3LOG_BENCH_PERFORMANCE=ON -DADD_G3LOG_UNIT_TEST=ON ..
 
 makeArg=`grep -c ^processor /proc/cpuinfo`    
 
