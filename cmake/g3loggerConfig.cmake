f2e9413a903eedf920be19b57cf06bcfd722063a
myd7349 <myd7349@gmail.com>
2019-04-06 12:25:40 +0800

    Improve CMake module file

--- cmake/g3loggerConfig.cmake
+++ cmake/g3loggerConfig.cmake
@@ -16,42 +16,41 @@
 # * ``G3LOG_FOUND`` -- check if the lib was found without using the newer ``if(TARGET g3logger)...``
 
 include(FindPackageHandleStandardArgs)
-find_path(G3LOG_INCLUDE_DIR 
-    NAMES
-         g3log/active.hpp
-         g3log/atomicbool.hpp
-         g3log/crashhandler.hpp
-         g3log/filesink.hpp
-         g3log/future.hpp
-         g3log/g3log.hpp
-         g3log/generated_definitions.hpp
-         g3log/logcapture.hpp
-         g3log/loglevels.hpp
-         g3log/logmessage.hpp
-         g3log/logworker.hpp
-         g3log/moveoncopy.hpp
-         g3log/shared_queue.hpp
-         g3log/sinkhandle.hpp
-         g3log/sink.hpp
-         g3log/sinkwrapper.hpp
-         g3log/stacktrace_windows.hpp
-         g3log/stlpatch_future.hpp
-         g3log/time.hpp
-    PATHS        
-        ENV G3LOG
-)
-find_library(G3LOG_LIBRARY
-            NAMES libg3logger g3logger )
-find_package_handle_standard_args(g3logger
+
+@PACKAGE_INIT@
+
+if (NOT TARGET g3logger)
+  include("${CMAKE_CURRENT_LIST_DIR}/g3loggerTargets.cmake")
+
+  get_target_property(G3LOG_INCLUDE_DIR g3logger INTERFACE_INCLUDE_DIRECTORIES)
+
+  get_target_property(G3LOG_LIBRARY_DEBUG g3logger IMPORTED_IMPLIB_DEBUG)
+  if (G3LOG_LIBRARY_DEBUG MATCHES ".*-NOTFOUND")
+    get_target_property(G3LOG_LIBRARY_DEBUG g3logger IMPORTED_LOCATION_DEBUG)
+  endif ()
+
+  get_target_property(G3LOG_LIBRARY_RELEASE g3logger IMPORTED_IMPLIB_RELEASE)
+  if (G3LOG_LIBRARY_RELEASE MATCHES ".*-NOTFOUND")
+    get_target_property(G3LOG_LIBRARY_RELEASE g3logger IMPORTED_LOCATION_RELEASE)
+  endif ()
+
+  if (NOT G3LOG_LIBRARY_DEBUG MATCHES ".*-NOTFOUND" AND
+      NOT G3LOG_LIBRARY_RELEASE MATCHES ".*-NOTFOUND")
+    set(G3LOG_LIBRARY
+      debug ${G3LOG_LIBRARY_DEBUG}
+      optimized ${G3LOG_LIBRARY_RELEASE}
+    )
+  elseif (NOT G3LOG_LIBRARY_DEBUG MATCHES ".*-NOTFOUND")
+    set(G3LOG_LIBRARY ${G3LOG_LIBRARY_DEBUG})
+  elseif (NOT G3LOG_LIBRARY_RELEASE MATCHES ".*-NOTFOUND")
+    set(G3LOG_LIBRARY ${G3LOG_LIBRARY_RELEASE})  
+  endif ()
+endif ()
+
+find_package_handle_standard_args(G3LOG
             REQUIRED_VARS G3LOG_INCLUDE_DIR G3LOG_LIBRARY)
 mark_as_advanced(G3LOG_INCLUDE_DIR G3LOG_LIBRARY)
 set(G3LOG_LIBRARIES ${G3LOG_LIBRARY})
 set(G3LOG_INCLUDE_DIRS ${G3LOG_INCLUDE_DIR})
 
-add_library(g3logger SHARED IMPORTED)
 find_package(Threads REQUIRED)
-set_target_properties(g3logger PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${G3LOG_INCLUDE_DIRS}"
-    IMPORTED_LOCATION "${G3LOG_LIBRARY}"
-    IMPORTED_LINK_INTERFACE_LIBRARIES Threads::Threads
-    )
