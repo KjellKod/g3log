KjellKod/g3log

commit 6c6122fafc79e92fc94a851b3ff83f87e8b80398

optionable to buffer x messages before writing to file, default to 100 which will really boost performance (#471)

src/filesink.cpp
@@ -14,13 +14,15 @@
 namespace g3 {
    using namespace internal;
 
-   FileSink::FileSink(const std::string &log_prefix, const std::string &log_directory, const std::string& logger_id)
+   FileSink::FileSink(const std::string &log_prefix, const std::string &log_directory, const std::string& logger_id, size_t write_to_log_every_x_message)
       : _log_details_func(&LogMessage::DefaultLogDetailsToString)
       ,_log_file_with_path(log_directory)
       , _log_prefix_backup(log_prefix)
       , _outptr(new std::ofstream)
       , _header("\t\tLOG format: [YYYY/MM/DD hh:mm:ss uuu* LEVEL FILE->FUNCTION:LINE] message\n\n\t\t(uuu*: microseconds fractions of the seconds value)\n\n")
       , _firstEntry(true)
+      , _write_counter(0)
+      , _write_to_log_every_x_message(write_to_log_every_x_message)
    {
       _log_prefix_backup = prefixSanityFix(log_prefix);
       if (!isValidFilename(_log_prefix_backup)) {
@@ -42,10 +44,12 @@ namespace g3 {
 
 
    FileSink::~FileSink() {
-      std::string exit_msg {"g3log g3FileSink shutdown at: "};
+      std::string exit_msg = {"g3log g3FileSink shutdown at: "};
       auto now = std::chrono::system_clock::now();
       exit_msg.append(localtime_formatted(now, internal::time_formatted)).append("\n");
-      filestream() << exit_msg << std::flush;
+
+      // write anything buffered up and then end with the exit msg
+      filestream() << _write_buffer << exit_msg << std::flush;
 
       exit_msg.append("Log file at: [").append(_log_file_with_path).append("]\n");
       std::cerr << exit_msg << std::flush;
@@ -59,7 +63,13 @@ namespace g3 {
       }
 
       std::ofstream &out(filestream());
-      out << message.get().toString(_log_details_func) << std::flush;
+      auto data =  message.get().toString(_log_details_func);
+
+      _write_buffer.append(data);
+      if (++_write_counter % _write_to_log_every_x_message == 0) {
+         out << message.get().toString(_log_details_func) << std::flush;
+         _write_buffer.clear();
+      }
    }
 
    std::string FileSink::changeLogFile(const std::string &directory, const std::string &logger_id) {

src/g3log/filesink.hpp
@@ -15,7 +15,7 @@ namespace g3 {
 
    class FileSink {
    public:
-      FileSink(const std::string &log_prefix, const std::string &log_directory, const std::string &logger_id="g3log");
+      FileSink(const std::string &log_prefix, const std::string &log_directory, const std::string &logger_id="g3log", size_t write_to_log_every_x_message = 100);
       virtual ~FileSink();
 
       void fileWrite(LogMessageMover message);
@@ -27,12 +27,14 @@ namespace g3 {
 
    private:
       LogMessage::LogDetailsFunc _log_details_func;
-
       std::string _log_file_with_path;
       std::string _log_prefix_backup; // needed in case of future log file changes of directory
       std::unique_ptr<std::ofstream> _outptr;
       std::string _header;
       bool _firstEntry;
+      std::string _write_buffer;
+      size_t _write_counter;
+      size_t _write_to_log_every_x_message;
 
       void addLogFileHeader();
       std::ofstream &filestream() {

commit 4f1224b9d52d7bfe74fde2bf31f88733ce04d19d

Fix filesink drop messages  improved (#479)
* Fix FileSink dropping messages introduced by 'optionable buffer to x messages' feature
Commit 6c6122fafc79e92fc94a851b3ff83f87e8b80398 introduced a bug where 99 out of 100 logs were actually dumped

* Updated unit tests to trigger sink flush bug
---------

Co-authored-by: Ryan Ammoury <ryan.ammoury@squadrone-system.com>

CMakeLists.txt
@@ -68,7 +68,7 @@ endif()
 
 # Calculate the version number
 SET(MAJOR_VERSION 2)
-SET(MINOR_VERSION 2)
+SET(MINOR_VERSION 3)
 
 IF ( NOT VERSION )
    IF ( "${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Windows" )

src/filesink.cpp
@@ -62,12 +62,11 @@ namespace g3 {
          _firstEntry = false;
       }
 
-      std::ofstream &out(filestream());
       auto data =  message.get().toString(_log_details_func);
 
       _write_buffer.append(data);
       if (++_write_counter % _write_to_log_every_x_message == 0) {
-         out << message.get().toString(_log_details_func) << std::flush;
+         filestream() << _write_buffer << std::flush;
          _write_buffer.clear();
       }
    }

test_unit/testing_helpers.cpp
@@ -26,7 +26,8 @@ namespace testing_helpers {
    std::string g_mockFatal_message = {};
    int g_mockFatal_signal = -1;
    bool g_mockFatalWasCalled = false;
-
+   const size_t kFlushToDiskWithThisInterval = 2;
+   
    std::string mockFatalMessage() {
       return g_mockFatal_message;
    }
@@ -110,7 +111,8 @@ namespace testing_helpers {
    }
 
    RestoreFileLogger::RestoreFileLogger(std::string directory)
-   : _scope(new ScopedLogger), _handle(_scope->get()->addSink(std::make_unique<g3::FileSink>("UNIT_TEST_LOGGER", directory), &g3::FileSink::fileWrite)) {
+   : _scope(new ScopedLogger), _handle(_scope->get()->addSink(std::make_unique<g3::FileSink>("UNIT_TEST_LOGGER", 
+         directory, "g3log", kFlushToDiskWithThisInterval), &g3::FileSink::fileWrite)) {
       using namespace g3;
       g3::initializeLogging(_scope->_currentWorker.get());
       clearMockFatal();
@@ -133,7 +135,7 @@ namespace testing_helpers {
       reset();
 
       if (!removeFile(_log_file))
-         ADD_FAILURE();
+        ADD_FAILURE();
    }
 
    std::string RestoreFileLogger::logFile() {

test_unit/test_io.cpp
@@ -155,12 +155,16 @@ TEST(Basics, Shutdown) {
    std::string file_content;
    {
       RestoreFileLogger logger(log_directory);
+      LOG(INFO) << "First message buffered, then flushed";
+      LOG(INFO) << "Second message still in the buffer";
       LOG(INFO) << "Not yet shutdown. This message should make it";
       logger.reset(); // force flush of logger (which will trigger a shutdown)
       LOG(INFO) << "Logger is shutdown,. this message will not make it (but it's safe to try)";
       file_content = readFileToText(logger.logFile()); // logger is already reset
       SCOPED_TRACE("LOG_INFO"); // Scope exit be prepared for destructor failure
    }
+   EXPECT_TRUE(verifyContent(file_content, "First message buffered, then flushed"));
+   EXPECT_TRUE(verifyContent(file_content, "Second message still in the buffer"));
    EXPECT_TRUE(verifyContent(file_content, "Not yet shutdown. This message should make it"));
    EXPECT_FALSE(verifyContent(file_content, "Logger is shutdown,. this message will not make it (but it's safe to try)"));
 }