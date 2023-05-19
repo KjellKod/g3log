26cb51217c060b70b36f8d14a1c8c3d61d8a8939
CryptoManiac <CryptoManiac@users.noreply.github.com>
2023-05-19 05:04:51 +0300

    Update loglevels.hpp
    See #495 for details

--- src/g3log/loglevels.hpp
+++ src/g3log/loglevels.hpp
@@ -35,9 +35,6 @@ struct LEVELS {
 
    LEVELS(const LEVELS& other): value(other.value), text(other.text.c_str()) {}
 
-   #if __cplusplus >= 202002L
-      constexpr
-   #endif
    LEVELS(int id, const std::string& idtext) : value(id), text(idtext) {}
 
    bool operator==(const LEVELS& rhs)  const {
@@ -94,11 +91,7 @@ namespace g3 {
    static const int kInternalFatalValue = 2000;
 } // g3
 
-#if __cplusplus >= 202002L
-   constexpr
-#else
-   const
-#endif
+const
 LEVELS G3LOG_DEBUG{g3::kDebugValue, "DEBUG"},
    INFO {g3::kInfoValue, "INFO"},
    WARNING {g3::kWarningValue, "WARNING"},
@@ -137,11 +130,7 @@ namespace g3 {
 
 namespace g3 {
    namespace internal {
-      #if __cplusplus >= 202002L
-         constexpr
-      #else
-         const
-      #endif
+      const
       LEVELS CONTRACT {g3::kInternalFatalValue, {"CONTRACT"}},
             FATAL_SIGNAL {g3::kInternalFatalValue + 1, {"FATAL_SIGNAL"}},
             FATAL_EXCEPTION {kInternalFatalValue + 2, {"FATAL_EXCEPTION"}};
